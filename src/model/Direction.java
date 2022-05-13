10a8d5acebc59b6872b8582e047d37b4568e6f0b
Safin Kasturi <safinkasturi2000@gmail.com>
2022-05-12 23:50:46 -0500

    modified direction enum and added doorStatus enum

--- src/model/Direction.java
+++ src/model/Direction.java
@@ -1,8 +1,25 @@
 package model;
 
 public enum Direction {
-  North,
-  South,
-  East,
-  West
+  NORTH(0, Direction.SOUTH),
+  EAST(1, Direction.WEST),
+  SOUTH(2, Direction.NORTH),
+  WEST(3, Direction.EAST);
+
+  private int index;
+  private Direction opposite;
+
+  Direction(int i, Direction d) {
+    index = i;
+    opposite = d;
+  }
+
+  public int getIndex() {
+    return index;
+  }
+
+  public Direction getOpposite() {
+    return opposite;
+  }
+
 }
