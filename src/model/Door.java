6abc4d68ce3c97a016a66dc0101807acd3338b7e
Cppenna2 <chrispenna2@gmail.com>
2022-05-13 16:11:49 -0500

    implemented door, room, and maze classes

--- src/model/Door.java
+++ src/model/Door.java
@@ -1,5 +1,46 @@
 package model;
 
 public class Door {
+    private Question myQuestion;
+    private DoorStatus doorStatus;
 
+    public Door(Question question) {
+      myQuestion = question;
+      doorStatus = DoorStatus.LOCKED;
+    }
+
+    public DoorStatus getStatus() {
+      return doorStatus;
+    }
+
+    public boolean isOpen() {
+      return doorStatus == DoorStatus.OPEN;
+    }
+
+    public boolean isLocked() {
+      return doorStatus == DoorStatus.LOCKED;
+    }
+
+    public boolean isGone() {
+      return doorStatus == DoorStatus.GONE;
+    }
+
+    public void open(String userAnswer) {
+      if (doorStatus == DoorStatus.LOCKED) {
+        if (myQuestion.validate(userAnswer)) {
+          doorStatus = DoorStatus.OPEN;
+        }
+        else {
+          doorStatus = DoorStatus.GONE;
+        }
+      }
+    }
+
+    public Question getQuestion() {
+      return myQuestion;
+    }
+
+    public cheatOpen() {
+      doorStatus = DoorStatus.OPEN;
+    }
 }
